d9d5d8b47a4544a7d1f9e57af51ade0038d02020
ClPhillips10 <Lakephillips3232@gmail.com>
2023-06-29 07:44:37 -0500

    Complete

--- app.js
+++ app.js
@@ -4,7 +4,24 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = Lake;
+let lastName;
+var age;
 // Exercise 2
-
+lastName = Phillips;
+age = 22;
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let inStock = false;
+let selectedSize = "m";
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
